284d330574ea9f16c68e12cc426c890236e879c3
Erik Yael Torres De Luna <yael_eriktorres@hotmail.com>
2023-09-18 11:33:51 -0600

    implementacion del servicio de correo

--- SistemaVenta.IOC/Dependencia.cs
+++ SistemaVenta.IOC/Dependencia.cs
@@ -10,8 +10,8 @@ using SistemaVenta.DAL.DBContext;
 using Microsoft.EntityFrameworkCore;
 using SistemaVenta.DAL.Interfaces;
 using SistemaVenta.DAL.Implementacion;
-//using SistemaVenta.BLL.Implementacion;
-//using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.BLL.Implementacion;
+using SistemaVenta.BLL.Interfaces;
 
 namespace SistemaVenta.IOC
 {
@@ -27,6 +27,8 @@ namespace SistemaVenta.IOC
 
             services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddScoped<IVentaRepository, VentaRepository>();
+
+            services.AddScoped<ICorreoService, CorreoService>();
 
         }
     }
